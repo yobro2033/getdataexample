a883fb02d6eb32e1433280f64ba9cfd9e0322be6
eniompw <71254710+eniompw@users.noreply.github.com>
2020-11-12 16:05:48 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# A very simple Flask Hello World app for you to get started with...
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/save')
+def save():
+    msg = request.args.get('msg', '')
+    f = open("messages.txt", "a")
+    f.write(msg + '\n')
+    f.close()
+    f = open("messages.txt", "r")
+    return f.read()
